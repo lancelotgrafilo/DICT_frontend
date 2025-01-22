5b5408634ef60ce562a80816f6ac5034f9304825
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-22 11:57:25 +0800

    added modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
-import { TrainingPage } from './pages/TrainingPage/TrainingPage.jsx'
+// import { TrainingPage } from './pages/TrainingPage/TrainingPage.jsx'
 import { SignUpPage } from './pages/SignUpPage/SignUpPage.jsx' 
 import { LoginPage } from "./pages/LoginPage/LoginPage.jsx"
+import { Sidebar } from './components/Sidebar/Sidebar.jsx'
 
 export default function App() {
   return (
     <>
-      <TrainingPage />
-
+      {/* <TrainingPage /> */}
+      <Sidebar/>
       <LoginPage/>
       <SignUpPage />
     </>
