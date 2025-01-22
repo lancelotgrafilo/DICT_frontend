e25257a0a717c4e8e0c5cb14f1d4fea6ceb89e5d
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-22 09:54:11 +0800

    fixed-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import { TrainingPage } from './pages/TrainingPage/TrainingPage.jsx'
 import { SignUpPage } from './pages/SignUpPage/SignUpPage.jsx' 
+import { LoginPage } from "./pages/LoginPage/LoginPage.jsx"
 
 export default function App() {
   return (
     <>
       <TrainingPage />
+
+      <LoginPage/>
       <SignUpPage />
     </>
   )
