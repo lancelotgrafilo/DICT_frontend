103d5e6fa9803e0fcd6175d753746fa82f78a7b1
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-27 09:49:24 +0800

    added home

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-// Import necessary components
-// import { RegionUserPage } from './pages/RegionUserPage/RegionUserPage.jsx';
-// import { RegionDashboard } from './components/RegionUserDashboard/RegionDashboard.jsx';
-// import { AppProviders } from './utils/context/AppProviders.jsx';
-// import { Modules } from './components/Modules/Modules.jsx';
-// import { Request } from './components/Request/Request.jsx';
-// import { Activities } from './components/Activities/Activities.jsx';
-// import { Settings } from './components/Settings/Settings.jsx';
+
 import { Homepage } from './pages/HomePage/HomePage.jsx';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
   return (
-    // <AppProviders>
+   
       <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
-
           <Route path="home" element={ <Homepage/> }></Route>
-          {/* <Route path="/" element={<RegionUserPage />}>
-            <Route path="dashboard" element={<RegionDashboard />} />
-            <Route path="request" element={<Request />} />
-            <Route path="activities" element={<Activities />} />
-            <Route path="modules" element={<Modules />} />
-            <Route path="settings" element={<Settings />} />
-          </Route> */}
         </Routes>
       </Router>
-    // </AppProviders>
+    
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,10 @@
 import styleHeader from './header.module.css';
 import dict from '../../assets/logo/DICT_pic.png';
-
-import { useContext } from 'react';
-import { ActiveButtonContext } from '../../utils/context/ActiveButtonContext';
-
 export function Header(){
-
-  const { activeButton } = useContext(ActiveButtonContext);
-
-  const renderHeading = () => {
-    switch (activeButton) {
-      case 'dashboard':
-        return 'Dashboard';
-      case 'request':
-        return '';
-      case 'activities':
-        return 'Activities';
-      case 'modules':
-        return "Modules";
-      case 'settings':
-        return 'Settings';
-      default:
-        return 'Dashboard';
-    }
-  };
-
-
 
   return (
     <div className={styleHeader.mainContent}>
       <div className={styleHeader.horizontalMenu}>
-        <div className={styleHeader.dashboardEntrance}>
-          <h1>{renderHeading()}</h1>
-        </div>
         <div className={styleHeader.imageContainer}>
           <img src={dict} alt="DICT" className={styleHeader.dict}/>
         </div>
