87ce9eb27751f2c12d5c905627cf237d11a4e153
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-27 14:53:44 +0800

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Homepage } from './pages/HomePage/HomePage.jsx';
-
+import { RequestForm } from './components/RequestForm/RequestForm.jsx';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
@@ -10,6 +10,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="home" element={ <Homepage/> }></Route>
+          <Route path="request-form" element={ <RequestForm/> }> </Route>
         </Routes>
       </Router>
     
