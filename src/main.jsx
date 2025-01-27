bbfa0edd39de3d25bf495a8ce9417ab26a17ad08
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-27 14:19:17 +0800

    added toasts

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './globals.css'
 import App from './App.jsx'
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+      transition={Slide}
+    />
   </React.StrictMode>
 )
