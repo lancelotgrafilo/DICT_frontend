130767552617bac5aaf503d931854b0f09e4db1d
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-27 09:31:28 +0800

    added home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import styleHomePage from "./homePage.module.css";
+
+export function Homepage () {
+  return(
+    <div className={styleHomePage.mainContent}>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
