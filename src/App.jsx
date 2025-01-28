db501b23081cc7aadf3ff6bd3eda98145fb09d12
Lancelot Grafilo <olifarg12345@gmail.com>
2025-01-28 10:44:07 +0800

    added request for admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@
 import { Homepage } from './pages/HomePage/HomePage.jsx';
 import { RequestForm } from './components/RequestForm/RequestForm.jsx';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { RegionUserPage } from './pages/RegionUserPage/RegionUserPage.jsx'; 
+import { AppProviders } from './utils/context/AppProviders.jsx';
+import { Request } from "./components/Request/Request.jsx";
 
 export default function App() {
   return (
+    <AppProviders>
    
       <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="home" element={ <Homepage/> }></Route>
           <Route path="request-form" element={ <RequestForm/> }> </Route>
+          
+          <Route path="admin" element={<RegionUserPage />}>
+            <Route path="request" element={<Request />} />
+          </Route>
+          
         </Routes>
       </Router>
+
+    </AppProviders>
     
   );
 }
