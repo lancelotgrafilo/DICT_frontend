1b2b052f97d1262aff7eecde7d299c3280edd672
Sam-s4m <samygerminamaglente@gmail.com>
2025-02-20 16:35:10 +0800

    Updated CIE module, Divisions component, and added new images

--- src/components/Divisions/Divisions.jsx
+++ src/components/Divisions/Divisions.jsx
@@ -3,7 +3,9 @@ import { Header } from "../Header/Header";
 import { Link } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
 import dcdImg from "../../assets/img/dcd.png";
-
+import certPHLogo from "../../assets/img/CERT-PH.png";
+import PnpkiLogo from "../../assets/img/pnpki.jpg";
+import CyberImg from "../../assets/img/Cyber.png";
 export function Divisions() {
   return (
     <>
@@ -17,7 +19,7 @@ export function Divisions() {
           <Link to="/divisions/cie" className={styleDivisions.cardLink}>
             <div className={styleDivisions.card}>
               <img
-                src="https://via.placeholder.com/350x200" // Replace with actual image URL
+                src={CyberImg}
                 alt="CIECSD Image"
                 className={styleDivisions.image}
               />
@@ -35,7 +37,7 @@ export function Divisions() {
           <a href="https://sites.google.com/dict.gov.ph/pnpki/ors" className={styleDivisions.cardLink}>
             <div className={styleDivisions.card}>
               <img
-                src={dcdImg} // Replace with actual image URL
+                src={PnpkiLogo}
                 alt="DCD Image"
                 className={styleDivisions.image}
               />
@@ -51,14 +53,14 @@ export function Divisions() {
           <a href="https://www.ncert.gov.ph/" className={styleDivisions.cardLink}>
             <div className={styleDivisions.card}>
               <img
-                src="https://via.placeholder.com/350x200" // Replace with actual image URL
-                alt="NCERT Image"
+                src={certPHLogo} 
+                alt="CERT-PH Logo"
                 className={styleDivisions.image}
               />
               <h2 className={styleDivisions.title}>
                 National Computer Emergency Response Team Division
               </h2>
-              <p className={styleDivisions.abbreviation}>(NCERTD)</p>
+              <p className={styleDivisions.abbreviation}>(NCERT)</p>
               <p className={styleDivisions.description}>
                 Handles national-level cybersecurity incidents and provides emergency response.
               </p>
